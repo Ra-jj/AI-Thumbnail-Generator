071655c22c14d5f26012235e9e5a439bf4d1e215
Monodip Bhattacharya <monodipkolkata@gmail.com>
2026-06-13 21:23:52 +0530

    first commit

---
+++ backend/routes/AuthRoutes.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { register } from "node:module";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+  verifyUser,
+} from "../controllers/AuthControllers.js";
+import protect from "../middlewares/auth.js";
+
+const AuthRouter = express.Router();
+
+AuthRouter.post("/register", registerUser);
+AuthRouter.post("/login", loginUser);
+AuthRouter.post("/verify", protect, verifyUser);
+AuthRouter.post("/logout", protect, logoutUser);
+
+export default AuthRouter;
